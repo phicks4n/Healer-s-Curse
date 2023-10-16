c1922683c9434d6b67e682e5d718ed524bb9bff5
Novasplode <jdrodriguez779@gmail.com>
2023-10-15 22:08:40 -0400

    More Dialogue

---
+++ Healers Curse/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Visual Cue")]
+    [SerializeField] private GameObject visualCue;
+
+    [Header("INK JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+    private bool playerInRange;
+
+    private void Awake()
+    {
+        playerInRange = false;
+        visualCue.SetActive(false);
+
+    }
+
+    private void Update()
+    {
+        if (playerInRange)
+        {
+            visualCue.SetActive(true);
+          
+        }
+        else 
+        {
+            visualCue.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInRange=true;
+        
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") 
+        {
+            playerInRange=false;
+        }
+        
+    }
+
+
+}
