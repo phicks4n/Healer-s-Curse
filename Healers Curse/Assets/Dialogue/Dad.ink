3056d49a89a5a57dd89a0f5c6494ff1e0502409d
Novasplode <jdrodriguez779@gmail.com>
2023-10-26 17:09:55 -0400

    Adding Choices to Dialogue

---
+++ Healers Curse/Assets/Dialogue/Dad.ink
@@ -0,0 +1,12 @@
+-> main
+
+===main===
+Are you ready for your final training?
+    +[Yes]
+    -> chosen("Yes")
+    +[No]
+    ->chosen("No")
+
+=== chosen(answer) ===
+You chose {answer}...
+-> END
--- Healers Curse/Assets/Scripts/Dialogue/DialogueManager.cs
+++ Healers Curse/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -11,6 +11,9 @@ public class DialogueManager : MonoBehaviour
     [Header("Dialogue UI")]
     [SerializeField] private GameObject dialoguePanel;
     [SerializeField] private TextMeshProUGUI dialogueText;
+    [Header("Choices UI")]
+    [SerializeField] private GameObject[] choices;
+    private TextMeshProUGUI[] choicesText;
 
     private Story currentStory;
 
@@ -34,6 +37,13 @@ public class DialogueManager : MonoBehaviour
     {
         dialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach (GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
     }
 
     private void Update()
@@ -72,10 +82,35 @@ public class DialogueManager : MonoBehaviour
         if (currentStory.canContinue)
         {
             dialogueText.text = currentStory.Continue();
+            DisplayChoices();
         }
         else
         {
             ExitDialogueMode();
+        }
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        if(currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("Greater choices than allowed. Number of choices given: "
+                + currentChoices.Count);
+        }
+
+        int index = 0;
+        foreach (Choice choice in currentChoices)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+
+        for(int i = index; i < choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
         }
     }
 }
