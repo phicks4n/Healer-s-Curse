6acf6da204b2724d2f557ef876193c7ddd04c02b
AndrewZ496 <144551615+AndrewZ496@users.noreply.github.com>
2023-11-18 21:38:52 -0500

    Update Enemy.cs

--- Healers Curse/Assets/Scripts/Combat/Enemy.cs
+++ Healers Curse/Assets/Scripts/Combat/Enemy.cs
@@ -7,22 +7,25 @@ public class Enemy : MonoBehaviour
     public string enemyName;
     public int enemyUnitLevel;
 
-    public int maxHP;
-    public int currentHP;
+    public float maxHP;
+    public float currentHP;
     public int armor;
 
-    public int damage;
+    public float damage;
+    public float damageTaken;
 
     // parameters are character dmg, enemy armor, characterCurrentHealth, characterMaxHealth
-    public bool TakeDamage(int dmg, int armor, int currentHealth, int maxHealth)
+    public bool TakeDamage(float dmg, int armor, float currentHealth, float maxHealth)
     {
         if (((currentHealth / maxHealth) * 100) >= .3 * maxHealth)
         {
-            currentHP = currentHP - (dmg - (int)(.5 * armor));
+            currentHP = currentHP - (dmg - (float)(.4 * armor));
+            damageTaken = (dmg - (float)(.4 * armor));
         }
         else if (((currentHealth / maxHealth) * 100) < .3 * maxHealth)
         {
-            currentHP = currentHP - (dmg - (int)(.15 * armor));
+            currentHP = currentHP - (dmg - (float)(.15 * armor));
+            damageTaken = (dmg - (float)(.15 * armor));
         }
 
         if (currentHP <= 0)
